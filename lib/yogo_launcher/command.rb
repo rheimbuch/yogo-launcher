a8e55351453be2e46b23e8637366461a6bce918e
Ryan Heimbuch <rheimbuch@gmail.com>
2010-02-05 16:28:58 -0700

    complete YogoLauncher::Command#create app creation

--- lib/yogo_launcher/command.rb
+++ lib/yogo_launcher/command.rb
@@ -1,4 +1,5 @@
 require 'thor'
+load File.join(YogoLauncher::YOGO_LAUNCHER_HOME,'tasks','yogo.thor')
 module YogoLauncher
     class Command < Thor
       include Thor::Actions
@@ -35,14 +36,11 @@ module YogoLauncher
           persvr = Persvr::Command.new
           persvr.invoke(:create, ['persvr'])
           
-          # puts "Source Paths: #{source_paths}"
-          #           puts destination_root
-          #           puts relative_to_original_destination_root(destination_root, false)
-          #           puts find_in_source_paths('resource/yogo_app')
-          #           directory 'yogo_app'
+          yogo = Yogo::Build.new(['yogo'], :type => 'package')
+          yogo.invoke(:complete)
+          create_file 'yogo/jettr.yml', FILES['yogo_jettr.yml']
         end
-        directory 'yogo_app', File.join(path,'yogo')
-        copy_file 'yogo_jettr.yaml', File.join(path,'yogo','jettr.yaml')
+        
       end
       
 FILES = {}
